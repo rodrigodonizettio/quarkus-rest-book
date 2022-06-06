188a80fca2629f6f604fef97006cd355d6e96e43
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-06-06 08:10:47 -0300

    Initial commit (Quarkus app + RESTEasy JSONB extension)

---
+++ src/test/java/br/com/rodrigodonizettio/BookResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.rodrigodonizettio;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class BookResourceIT extends BookResourceTest {
+
+    // Execute the same tests but in native mode.
+}
