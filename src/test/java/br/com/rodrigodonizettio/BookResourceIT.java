7dbd8e178558e74633ba732fbf117739578f1108
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2022-06-09 09:08:59 -0300

    fix() Updating Native tests

--- src/test/java/br/com/rodrigodonizettio/BookResourceIT.java
+++ src/test/java/br/com/rodrigodonizettio/BookResourceIT.java
@@ -1,9 +1,34 @@
 package br.com.rodrigodonizettio;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusIntegrationTest
 public class BookResourceIT extends BookResourceTest {
-
     // Execute the same tests but in native mode.
+
+    // mvn verify -Pnative (it'll execute mvn test, then will build a native app and then run tests against the binary)
+    // The binary is built under %prod profile, so some tests may be overridden to match your application.profile configs.
+
+    @Test
+    public void getAllBooksTest() {
+        given()
+                .when().get("/book")
+                .then()
+                .statusCode(200)
+                .body(is("[{\"id\":1,\"isTranslated\":\"true\",\"numberOfPages\":272,\"title\":\"Essentialism\"}," +
+                        "{\"id\":2,\"isTranslated\":\"true\",\"numberOfPages\":295,\"title\":\"The Hard Things About Hard Things\"}]"));
+    }
+
+    @Test
+    public void getBookByExistingIdTest() {
+        given()
+                .when().get("/book/2")
+                .then()
+                .statusCode(200)
+                .body(is("{\"id\":2,\"isTranslated\":\"true\",\"numberOfPages\":295,\"title\":\"The Hard Things About Hard Things\"}"));
+    }
 }
